58a4f8456adbf367c26117825ed94e52e0a49ad4
Lucas <staroscky60@gmail.com>
2022-06-13 15:21:13 -0300

    refactoring functions

--- src/enums/PossibleAnswers.ts
+++ src/enums/PossibleAnswers.ts
@@ -1,7 +1,8 @@
 enum PossibleAnswer {
-    RATE_MOVIE = '1',
-    SHOW_WITH_AVERAGE = '2',
-    ADD_MOVIE_TO_LIST = '3',
+    DOWNLOAD = '1',
+    RATE_MOVIE = '2',
+    SHOW_WITH_AVERAGE = '3',
+    ADD_MOVIE_TO_LIST = '4',
     EXIT = '0'
 }
 
--- src/index.ts
+++ src/index.ts
@@ -3,46 +3,136 @@ import Movie from "./interfaces/Movie";
 import User from "./interfaces/User";
 import MovieService from "./services/MovieService";
 import askCli from "./utils/askCli"
-import showMovies from "./utils/cli-options/showMovies";
-import answerCli from "./utils/cli-options/answer-cli";
-import listUsers from "./utils/cli-options/listUsers";
-import selectUser from "./utils/cli-options/selectUser";
-import rateMovie from "./utils/cli-options/rateMovie";
-import addMovieToList from "./utils/cli-options/addMoviesById";
-import showMoviesAverage from "./utils/cli-options/showMoviesAverage";
+import calculateMoviesAverage from "./utils/calculateMoviesAverage";
+import addFilms from "./utils/addMovies";
+import answerCli from "./utils/answerCli";
 
-const movieService = new MovieService();
+let movies: Movie[] = [];
+let loggedUserId: number;
+
 const users: User[] = [{
     id: 1,
     name: "Lucas starosky",
     age: 23,
     myList: []
-}]
+}];
 
-let movies: Movie[];
-let loggedUserId: number;
+const movieService = new MovieService();
 
-async function run() {
-    await showMovies(movies, movieService);
-    await listUsers(users);
-    await selectUser(loggedUserId, users);
-    console.log(loggedUserId)
-
-    const questions = askCli("input", "Digite uma opção: \n 1 - Dar avaliação \n 2 - Calcular média avaliação \n 3 - Adicionar filme a lista \n 0 - Sair \n");
+async function userOptions() {
+    const questions = askCli("input", "Digite uma opção: \n 1 - Download dos filmes \n 2 - Dar avaliação \n 3 - Calcular média avaliação \n 4 - Adicionar filme a lista \n 0 - Sair \n");
     const { option } = await answerCli(questions);
 
-    switch (option) {
+    return option;
+}
+
+async function listMovies() {
+    try {
+        console.log("Carregando Filmes...");
+        movies = await movieService.listAll();
+        console.log("Download concluído!");
+
+        movies.map((movie) => console.log(`${movie.id} - ${movie.name}`));
+
+    } catch (error) {
+        console.error("Erro ao carregar filme!");
+    } finally {
+        run();
+    }
+}
+
+async function listUsers() {
+    try {
+        console.log("Carregando usuários...");
+        users.map((user) => console.log(`${user.id} - ${user.name}`));
+
+    } catch (error) {
+        console.log("Erro ao listar usuários!");
+    }
+}
+
+async function selectUser() {
+    try {
+        const userQuestion = askCli("number", "Selecione um usuário para continuar:");
+        const userId = await answerCli(userQuestion);
+        loggedUserId = users.findIndex((user) => user.id === userId.option);
+    } catch (error) {
+        console.log("Erro ao selecionar usuário!")
+    }
+}
+
+async function rateMovie() {
+    try {
+        const chooseMovieQuestions = askCli("number", "Qual filme?");
+        const answersMovie = await answerCli(chooseMovieQuestions);
+        const movieId = answersMovie.option;
+
+        const rateQuestion = askCli("number", "Qual avaliacao de 0 a 5?")
+        const answerRatings = await answerCli(rateQuestion);
+        let rate = answerRatings.option;
+
+        movies.forEach((movie => {
+            if (movie.id === movieId) {
+                movie.ratings.push(rate);
+                console.log(movie);
+            }
+        }))
+
+    } catch (error) {
+        console.log("Erro ao avaliar filme!")
+    } finally {
+        run()
+    }
+}
+
+async function showMoviesAverage() {
+    try {
+        const moviesWithAverage = calculateMoviesAverage(movies);
+        moviesWithAverage.map((movie => console.log(`${movie.name} - ${movie.average}`)));
+        console.log(moviesWithAverage)
+    } catch (error) {
+        console.log("Erro ao listar avaliação média dos filmes!");
+    } finally {
+        run()
+    }
+}
+
+async function addMovieToList() {
+    try {
+        const addToListQuestion = askCli("input", "Digite o(s) id(s) do(s) filme(s): ex: (1, 2, 3, 4, 5)");
+        const answerId = await answerCli(addToListQuestion);
+
+        const ids = answerId.option
+            .split(",")
+            .map((id: string) => parseInt(id));
+
+        users[loggedUserId] = addFilms(users[loggedUserId], movies, ...ids);
+
+        console.log(users[loggedUserId]);
+    } catch (error) {
+        console.log("Erro ao adicionar filme(s) a lista!");
+    } finally {
+        run()
+    }
+}
+
+
+async function run() {
+    await listUsers();
+    await selectUser();
+
+    switch (await userOptions()) {
+        case PossibleAnswer.DOWNLOAD:
+            await listMovies();
+            break;
         case PossibleAnswer.RATE_MOVIE:
-            await rateMovie(movies);
-            run()
+            await rateMovie();
             break;
         case PossibleAnswer.SHOW_WITH_AVERAGE:
-            await showMoviesAverage(movies);
-            run()
+            await showMoviesAverage();
             break;
         case PossibleAnswer.ADD_MOVIE_TO_LIST:
-            await addMovieToList(users, loggedUserId, movies);
-            break;
+            return await addMovieToList();
         case PossibleAnswer.EXIT:
             return;
     }
